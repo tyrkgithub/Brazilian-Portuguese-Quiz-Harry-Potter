f929568db3403e357e1abe9077313035510c6af7
tyrk <tiayorke@hotmail.co.uk>
2024-04-04 19:15:05 +0100

    created questions for first paragraph

---
+++ README.md
@@ -0,0 +1,84 @@
+# <h1>Week Six Challenge - Code Quiz</h1>
+  <br/>
+ <h2>My Task</h2> 
+   <p>This weeks challenge is creating a timed quiz with javascript functions which saves the highscore.</p>
+  
+ <br/>
+  <ul>
+    <li><a href="https://github.com/tyrkgithub/Javascript-Knowledge-Quiz/blob/main/README.md#link">Link</a></li>
+    <li><a href="https://github.com/tyrkgithub/Javascript-Knowledge-Quiz/blob/main/README.md#acceptance-criteria">User Story</a></li>
+    <li><a href="https://github.com/tyrkgithub/Javascript-Knowledge-Quiz/blob/main/README.md#resources">Resources</a></li>
+    <li><a href="https://github.com/tyrkgithub/Javascript-Knowledge-Quiz/blob/main/README.md#visual-references">Visual References</a></li>
+    <li><a href="https://github.com/tyrkgithub/Javascript-Knowledge-Quiz/blob/main/README.md#notes">Notes</a></li>
+  
+  </ul>
+  <br/>
+  
+ <h2>Link</h2>
+  
+  <br/>
+  
+  <p> Please find the link to my deployed application <a href="https://tyrkgithub.github.io/Javascript-Knowledge-Quiz">here.</a> </p>
+  
+  <br/>
+  
+ <h2>User Story</h2>
+  
+  <br/>
+  
+  <p>I want to create a quiz on Javascript Fundementals to help me study and to gauge my progress compared to my peers.<br/>
+  </p>
+  
+  <br/>
+  
+<h2>Acceptance Criteria</h2>
+
+  <br/>
+  
+  <ul>
+    <li>A start button that when clicked a timer starts and the first question appears</li>
+    <li>Questions contain buttons for each answer</li>
+    <li>When answer is clicked, the next question appears</li>
+    <li>If the answer clicked was incorrect then subtract time from the clock</li>
+    <li>The quiz should end when all questions are answered or the timer reaches 0</li>
+    <li>When the game ends, it should display their score and give the user the ability to save their initials and their score</li>
+  </ul>
+  
+  <br/>
+
+<h2>Resources</h2>
+
+  <br/>
+ 
+  <p>This week to complete this task I utilised my one-on-one tutoring session to help me debug my code. I used w3schools to conduct further studies into JSON, getItem and setItem.<br/>
+ <br/>
+  
+  <br/>
+
+ <h2>Visual References</h2>
+ 
+  <br/>
+
+  <p>Below are screenshots of the quiz and the highscores pages.</p>
+
+<img width="1019" alt="Untitled" src="https://user-images.githubusercontent.com/118772733/214346981-5f11bd53-2a9a-4b5b-9b75-0d93767b0e4b.png">
+
+<img width="814" alt="Untitled 2" src="https://user-images.githubusercontent.com/118772733/214346985-fc8ff302-f965-4446-b125-492c0aadc576.png">
+
+
+
+  <br/>
+   
+ <h2>Notes</h2>
+ 
+
+  <br/>
+  
+  <p>In this part of my READ.ME page I include things which helped me to complete the project.</p>
+  
+  <br/>
+  
+  <ul>
+   <li>I was unable to get a decrease in time function to work but this was fixed when my time variable was declared outside of the count function</li>
+   <li>I tested out using a time bar for this project using code found on w3schools. Whilst this is interesting to play around with it is just a bit too advanced for me at this time</li>
+
---
+++ assets/questions.js
@@ -0,0 +1,211 @@
+let questions = [
+  {
+    Question: "What does “Pedra“ mean in English?",
+    option: ["Lake", "Castle", "Stone", "Pensive"],
+    correctAnswer: "Stone",
+  },
+  {
+    Question: "What does “Filosofal“ mean in English?",
+    option: ["Philosopher", "Parselmouth", "Patronus", "Firebolt"],
+    correctAnswer: "Philosopher",
+  },
+  {
+    Question: "How do you say “Stone“ in Brazillian Portuguese?",
+    option: ["Para", "Pelos", "Porta", "Pedra"],
+    correctAnswer: "Pedra",
+  },
+  {
+    Question: "How do you say “Philosopher“ in Brazillian Portuguese?",
+    option: ["Fechadura", "Filosofal", "Ficaria", "Pertencia"],
+    correctAnswer: "Filosofal",
+  },
+  {
+    Question: "What does “Capítulo“ mean in English?",
+    option: ["Chapter", "Centaur", "Chaser", "Chamber"],
+    correctAnswer: "Chapter",
+  },
+  {
+    Question: "How do you say “Chapter“ in Brazillian Portuguese?",
+    option: ["Contrariar", "Compras", "Capítulo", "Calcanhares"],
+    correctAnswer: "Capítulo",
+  },
+  {
+    Question: "What does “Menino“ mean in English?",
+    option: ["Magical", "Boy", "Men", "Bat"],
+    correctAnswer: "Boy",
+  },
+  {
+    Question: "How do you say “Boy“ in Brazillian Portuguese?",
+    option: ["Melhor", "Mundo", "Mais", "Menino"],
+    correctAnswer: "Menino",
+  },
+  {
+    Question: "What does “Sobreviveu“ mean in English?",
+    option: ["Spilching", "Salamader", "Survived", "Seeker"],
+    correctAnswer: "Survived",
+  },
+  {
+    Question: "How do you say “Survived“ in Brazillian Portuguese?",
+    option: ["Subiram", "Sozinho", "Sentado", "Sobreviveu"],
+    correctAnswer: "Sobreviveu",
+  },
+  {
+    Question: "What does “Sr e Sra“ mean in English?",
+    option: ["His and hers", "Survived", "To be or not to be", "Mr and Mrs"],
+    correctAnswer: "Mr and Mrs",
+  },
+  {
+    Question: "How do you say “Mr and Mrs“ in Brazillian Portuguese?",
+    option: ["Sir e Sar", "Sr e Sra", "En y Ena", "Nr e Nra"],
+    correctAnswer: "Sr e Sra",
+  },
+  {
+    Question: "What does “Da rua dos“ mean in English?",
+    option: [
+      "Here to there",
+      "Two of them",
+      "From the street",
+      "From the house",
+    ],
+    correctAnswer: "From the street",
+  },
+  {
+    Question: "How do you say “From the street“ in Brazillian Portuguese?",
+    option: ["Na esquina", "Da rua dos", "Da cidade", "Pela rua"],
+    correctAnswer: "Da rua dos",
+  },
+  {
+    Question: "What does “Alfeneiros“ mean in English?",
+    option: ["Privet", "Pivet", "Alfie", "Privet"],
+    correctAnswer: "Privet",
+  },
+  {
+    Question: "How do you say “Privet“ in Brazillian Portuguese?",
+    option: ["Achavam", "Alfeneiros", "Aguentar", "Aparecessem"],
+    correctAnswer: "Alfeneiros",
+  },
+  {
+    Question: "What does “Se orgulhavam“ mean in English?",
+    option: ["They were proud", "Come look", "Get organised", "The order"],
+    correctAnswer: "They were proud",
+  },
+  {
+    Question: "How do you say “They were proud“ in Brazillian Portuguese?",
+    option: ["Eles eram maus", "Ser feliz", "Se orgulhavam", "Orgulhoso de ti"],
+    correctAnswer: "Se orgulhavam",
+  },
+  {
+    Question: "What does “De dizer“ mean in English?",
+    option: ["The speaker", "The speach", "To say", "To do"],
+    correctAnswer: "To say",
+  },
+  {
+    Question: "How do you say “To say“ in Brazillian Portuguese?",
+    option: ["O discurso", "Pendência", "O orador", "De dizer"],
+    correctAnswer: "De dizer",
+  },
+  {
+    Question: "What does “Que eram perfeitamente normais“ mean in English?",
+    option: [
+      "They are the favourite",
+      "They are not perfect at all",
+      "Who want to be normal, not perfect",
+      "That they were perfectly normal",
+    ],
+    correctAnswer: "That they were perfectly normal",
+  },
+  {
+    Question:
+      "How do you say “That they were perfectly normal“ in Brazillian Portuguese?",
+    option: [
+      "Eles são os favoritos",
+      "Quem quer ser normal, não perfeito",
+      "Que eram perfeitamente normais",
+      "Eles não são nada perfeitos",
+    ],
+    correctAnswer: "Que eram perfeitamente normais",
+  },
+  {
+    Question: "What does “Eram as últimas pessoas“ mean in English?",
+    option: [
+      "They were the first men",
+      "The people were second",
+      "They were the last people",
+      "The people are first",
+    ],
+    correctAnswer: "They were the last people",
+  },
+  {
+    Question:
+      "How do you say “They were the last people“ in Brazillian Portuguese?",
+    option: [
+      "Eles foram os primeiros homens",
+      "Eram as últimas pessoas",
+      "O povo ficou em segundo lugar",
+      "As pessoas estão em primeiro lugar",
+    ],
+    correctAnswer: "Eram as últimas pessoas",
+  },
+  {
+    Question: "What does “No mundo que se“ mean in English?",
+    option: [
+      "Whole wide world",
+      "In the world that",
+      "The other planet",
+      "In the city",
+    ],
+    correctAnswer: "In the world that",
+  },
+  {
+    Question: "How do you say “In the world that“ in Brazillian Portuguese?",
+    option: ["No mundo que se", "O mundo todo", "Na cidade", "O outro planeta"],
+    correctAnswer: "No mundo que se",
+  },
+  {
+    Question:
+      "What does “Esperaria que se metessem alguma cois estranha ou misteriosa“ mean in English?",
+    option: [
+      "I would expect something to happen",
+      "I wouldn't expect something strange to happen",
+      "I would expect something strange and mysterious to happen",
+      "I would expect something wonderful to happen",
+    ],
+    correctAnswer: "I would expect something strange and mysterious to happen",
+  },
+  {
+    Question:
+      "How do you say “I would expect something strange and mysterious to happen“ in Brazillian Portuguese?",
+    option: [
+      "Esperaria que se metessem alguma cois estranha ou misteriosa",
+      "Eu esperaria que algo acontecesse",
+      "Esperaria que algo estranho acontecesse",
+      "Eesperaria que algo maravilhoso acontecesse",
+    ],
+    correctAnswer:
+      "Esperaria que se metessem alguma cois estranha ou misteriosa",
+  },
+  {
+    Question:
+      "What does “Porque simplesmente nao compactuavam com esse tipo de bobagem“ mean in English?",
+    option: [
+      "Because they always go along with this kind of nonsense",
+      "Because simply nonsense is bad",
+      "Because simply they just didn't go along with this kind of nonsense",
+      "Because simply it is all nonsense",
+    ],
+    correctAnswer:
+      "Because simply they just didn't go along with this kind of nonsense",
+  },
+  {
+    Question:
+      "How do you say “Because simply they just didn't go along with this kind of nonsense“ in Brazillian Portuguese?",
+    option: [
+      "Porque eles sempre concordam com esse tipo de bobagem",
+      "Porque simplesmente nao compactuavam com esse tipo de bobagem",
+      "Porque simplesmente é tudo bobagem",
+      "Porque simplesmente bobagem é ruim",
+    ],
+    correctAnswer:
+      "Porque simplesmente nao compactuavam com esse tipo de bobagem",
+  },
+];
